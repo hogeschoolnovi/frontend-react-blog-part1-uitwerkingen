6c843894562156ffec6ee5e3b083309517bf6ec2
novaeeken <nova_eeken@hotmail.com>
2023-09-21 18:30:55 +0200

    Setup base styling and implemented basic routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import {Link, Route, Routes, useNavigate} from 'react-router-dom';
+import Home from './pages/homePage/Home.jsx';
+import NewPost from './pages/newPostPage/NewPost.jsx';
+import Overview from './pages/overviewPage/Overview.jsx';
+import PostDetail from './pages/postDetailPage/PostDetail.jsx';
+import logoMedium from './assets/logo-medium.png'
 import './App.css'
-import logo from './assets/logo-white.png'
 
 function App() {
+    const navigate = useNavigate();
+
     return (
-        <div className="page-container">
-            <img src={logo} alt="Company logo"/>
-            <h1>Begin hier met het maken van jouw blog-applicatie!</h1>
-        </div>
+        <>
+            <nav className="main-navigation outer-content-container">
+                <div className="inner-nav-container">
+                    <button type="button" className="main-navigation-logo-button" onClick={() => navigate('/')}>
+                        <img src={logoMedium} alt="Logo that links to home page"/>
+                    </button>
+                    <ul className="main-navigation-links">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/posts">Alle posts</Link></li>
+                        <li><Link to="/new">Nieuwe post maken</Link></li>
+                    </ul>
+                </div>
+            </nav>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/new" element={<NewPost />} />
+                    <Route path="/posts" element={<Overview />} />
+                    <Route path="/posts/:id" element={<PostDetail />} />
+                </Routes>
+            </main>
+            <footer className="footer-navigation outer-content-container">
+                Blogventure &copy; 2023 - ontwikkeld voor NOVI Hogeschool
+            </footer>
+        </>
     )
 }
 
