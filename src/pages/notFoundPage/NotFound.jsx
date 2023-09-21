a490ab55313c4ce6b866c2e7bbe7ec209945725a
novaeeken <nova_eeken@hotmail.com>
2023-09-21 21:34:55 +0200

    Add blog detail page with icons

--- src/pages/notFoundPage/NotFound.jsx
+++ src/pages/notFoundPage/NotFound.jsx
@@ -5,12 +5,13 @@ function NotFound() {
     const navigate = useNavigate();
 
     return (
-        <section className="not-found-section outer-content-container__text-restriction">
+        <section className="not-found-section outer-content-container">
             <div className="inner-content-container">
                 <h1>404</h1>
                 <h2>De pagina waar je naar zoekt bestaat niet</h2>
-                <p>(Tenzij je opzoek was naar een pagina met een animatie van een taco die struikelt. Als dat zo is bestaat de pagina wel en heb je 'm zeker gevonden.)</p>
-                <button type="button" onClick={() => navigate('/')}>Terug naar home</button>
+                <span>
+                    <button type="button" onClick={() => navigate('/')}>Terug naar home</button>
+                </span>
             </div>
         </section>
     );
