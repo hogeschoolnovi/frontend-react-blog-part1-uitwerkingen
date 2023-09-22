866a441d4f708403a6ef3f7e5d89320e27a850c6
novaeeken <nova_eeken@hotmail.com>
2023-09-22 11:27:39 +0200

    Create new post form and required information that needs to be collected

--- src/pages/newPostPage/NewPost.jsx
+++ src/pages/newPostPage/NewPost.jsx
@@ -1,10 +1,83 @@
 import './NewPost.css';
+import {useState} from 'react';
+import calculateReadTime from '../../helpers/calculateReadTime.js';
 
 function NewPost() {
+    const [formState, setFormState] = useState({
+        title: '',
+        subtitle: '',
+        author: '',
+        content: '',
+    });
+
+    function handleChange(e) {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log({
+            ...formState,
+            shares: 0,
+            comments: 0,
+            created: new Date().toISOString(),
+            readTime: calculateReadTime(formState.content),
+        });
+    }
+
     return (
-        <>
-            <h1>New Post</h1>
-        </>
+        <section className="new-post-section outer-content-container">
+            <div className="inner-content-container__text-restriction">
+                <form className="new-post-form" onSubmit={handleSubmit}>
+                    <h1>Post toevoegen</h1>
+                    <label htmlFor="post-title">Titel</label>
+                    <input
+                        type="text"
+                        id="post-title"
+                        name="title"
+                        required
+                        value={formState.title}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="post-subtitle">Subtitle</label>
+                    <input
+                        type="text"
+                        id="post-subtitle"
+                        name="subtitle"
+                        required
+                        value={formState.subtitle}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="post-author">Naam en achternaam</label>
+                    <input
+                        type="text"
+                        id="post-author"
+                        name="author"
+                        required
+                        value={formState.author}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="post-content">Blogpost</label>
+                    <textarea
+                        name="content"
+                        id="post-content"
+                        cols="30"
+                        rows="10"
+                        required
+                        minLength={300}
+                        maxLength={2000}
+                        value={formState.content}
+                        onChange={handleChange}></textarea>
+                   <button type="submit">
+                        Toevoegen
+                    </button>
+
+                </form>
+            </div>
+        </section>
     );
 }
 
